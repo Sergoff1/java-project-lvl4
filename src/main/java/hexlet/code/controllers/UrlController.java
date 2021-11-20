153672d850defa5962396550456f10ac8c7a832d
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-11-20 17:22:34 +0300

    Add content for urls

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,10 +1,21 @@
 package hexlet.code.controllers;
 
+import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrl;
 import io.javalin.http.Handler;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
 public final class UrlController {
     public static Handler listUrls = ctx -> {
 
+        List<Url> urls = new QUrl()
+                .orderBy().id.asc()
+                .findList();
+
+        ctx.attribute("urls", urls);
         ctx.render("urls/index.html");
     };
 
@@ -13,6 +24,28 @@ public final class UrlController {
     };
 
     public static Handler createUrl = ctx -> {
+        try {
+            String authorityUrl = new URL(ctx.formParam("url")).getAuthority();
 
+            boolean urlExist = new QUrl()
+                    .name.equalTo(authorityUrl)
+                    .exists();
+
+            if (urlExist) {
+                ctx.sessionAttribute("flash", "Страница уже существует");
+                ctx.sessionAttribute("flash-type", "info");
+            } else {
+                new Url(authorityUrl).save();
+
+                ctx.sessionAttribute("flash", "Страница успешно добавлена");
+                ctx.sessionAttribute("flash-type", "success");
+            }
+        } catch (MalformedURLException e) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flash-type", "danger");
+            ctx.redirect("/");
+            return;
+        }
+        ctx.redirect("/urls");
     };
 }
